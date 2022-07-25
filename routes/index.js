d7518870498c371f4453f9b558d911cfdabfdf3d
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-07-25 10:52:06 -0600

    adds router.post function to show date

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/post-message", (req, res) => {
+  const dateTime = new Date().toLocaleDateString("en-US");
+  const clientMessage = req.body;
+  res.json(`Received client message: ${clientMessage}. Responded at ${dateTime.toString()}`)
+});
+
+module.exports = router;
