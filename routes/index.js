490ae4a22532ec73c23b1cd6d51f4663a668fac1
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-07-27 11:02:36 -0600

    adds router.post create-user tested with postman - success

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
 var express = require("express");
 var router = express.Router();
+const { uuid } = require("uuidv4");
+// npm i uuidv4
+
+const userList = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    email: "jd@gmail.com",
+  },
+];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -7,10 +18,40 @@ router.get("/", function (req, res, next) {
 });
 
 router.post("/post-message", (req, res) => {
-  const dateTime = new Date().toLocaleDateString("en-US");
-  const clientMessage = req.body.clientMessage
-  console.log(clientMessage);
-  res.json({serverMessage : `Received client message: ${clientMessage}. Responded at ${dateTime.toString()}`})
+  try {
+    const clientMessage = req.body.clientMessage;
+    const dateTime = new Date().toLocaleDateString("en-US");
+    const response = `Received client message ${clientMessage}. Responded at ${dateTime.toString()}`
+    res.json({ serverMessage : response }).status(200);
+  } catch (e) {
+    console.log(`Message not sent ${e}`, e);
+    res.json({ serverMessage: `${e}` }).status(500)
+  }
+});
+
+router.post("/create-user", async (req, res) => {
+  try {
+    const uid = uuid();
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const newUser = {
+      uid,
+      firstName,
+      lastName,
+      email,
+    };
+    userList.push(newUser);
+    console.log(userList);
+    res
+      .json({ serverMessage: `New user successfully created.`, success: true })
+      .status(200);
+  } catch (e) {
+    console.log(`Error user not created`, e);
+    res
+    .json({ serverMessage: `${e}`, success: false })
+    .status(500);
+  }
 });
 
 module.exports = router;
